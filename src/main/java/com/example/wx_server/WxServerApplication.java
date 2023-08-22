a978e10ce194e0e6061f230936db29c06843855a
tz6667 <142771030+tz6667@users.noreply.github.com>
2023-08-22 14:21:35 +0800

    创建了SpringBoot项目框架

---
+++ src/main/java/com/example/wx_server/WxServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.wx_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wx_server/WxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wx_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
